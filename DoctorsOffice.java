59ef7eee1b3666c13f88bab4e25a26790c95c8fe
daytonpe <daytonpe14@gmail.com>
2018-03-31 02:12:11 -0500

    all complete except graceful stop

--- DoctorsOffice.java
+++ DoctorsOffice.java
@@ -8,28 +8,36 @@ Due: 31 March 2018
 package doctorsoffice;
 
 
-// Java implementation of a producer and consumer
-// that use semaphores to control synchronization.
-
-import static java.lang.Thread.sleep;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Semaphore;
 
-class DoctorsOffice{
+public class DoctorsOffice{
+
     int item;
     int currentID;
     int patientInOfficeID;
-    List<Integer> waitingRoomList = new ArrayList<>();
+    List<Patient> waitroomList = new ArrayList<>();
+    List<Patient> exitedList   = new ArrayList<>();
+
+    Patient patientAtReceptionist;
+
+    int NUMPATIENTS;
+    int NUMDOCTORS;
+    int NUMNURSES;
+
+    Receptionist RECEPTIONIST;
+
+    boolean officeClosed = false;
 
     // semCon initialized with 0 permits
     // to ensure put() executes first
-    static  Semaphore semCon = new Semaphore(0);
+    static Semaphore semCon = new Semaphore(0);
     static Semaphore semProd = new Semaphore(1);
 
     //start with one receptionist
-    static Semaphore receptionistCon = new Semaphore(1);
-    static Semaphore receptionistProd = new Semaphore(0);
+    static Semaphore consumeReceptionistSem = new Semaphore(1);
+    static Semaphore produceReceptionistSem = new Semaphore(0);
 
     //waiting room queue
     static Semaphore waitingRoomSem = new Semaphore(0);
@@ -37,213 +45,269 @@ class DoctorsOffice{
     static Semaphore doctorSem = new Semaphore(0); //cons
     static Semaphore nurseSem = new Semaphore(1); //prod
 
+    // Doctors Office Constructor
+    DoctorsOffice(int p, int d){
+        NUMPATIENTS = p;
+        NUMDOCTORS = d;
+        RECEPTIONIST = new Receptionist();
 
-    public int getWaitingRoomListLength(){
-        return waitingRoomList.size();
-    }
-
-    public int getWaitingRoomNext(){
-        return waitingRoomList.get(0);
-    }
-
-    public boolean getReceptionist(int currentID){
-        try{
-            receptionistCon.acquire();
-            Thread.sleep(500);
-//            System.out.println("Patient "+currentID+" has checked in.");
-            this.currentID = currentID;
-        } catch(InterruptedException e) {
-            System.out.println("InterruptedException caught");
-        }
-        receptionistProd.release();
-        return true;
-    }
-
-    public void putReceptionist(){
-        try{
-            receptionistProd.acquire();
-            Thread.sleep(500);
-            System.out.println("Receptionist registers patient "+ currentID);
-        } catch(InterruptedException e) {
-            System.out.println("InterruptedException caught");
+        // Create Doctor and Nurse Threads
+        for (int i = 1; i <= d; i++) {
+            new Doctor(i);
         }
 
-        receptionistCon.release(); //okay the receptionist is free
-    }
-
-    // Patient enters waiting room queue
-    public void putWaitingRoom(int id){
-        waitingRoomSem.release();
-        waitingRoomList.add(id);
-//        System.out.println("Waiting Room Occupancy: "+ waitingRoomSem.availablePermits());
-//        System.out.println("Arraylist contains: " + waitingRoomList.toString());
-    }
-
-    public void nurseProduce(int NurseID){
-//        System.out.println("Put Nurse Called");
-
-        try{
-            waitingRoomSem.acquire();
-//            System.out.println("WAITINGROOM: "+waitingRoomList.size());
+        // Create Patient Threads
+        for (int i = 1; i <= p; i++) {
             try{
-                nurseSem.acquire();
-                int currentPatient = waitingRoomList.remove(0);
-                System.out.println("Nurse "+NurseID+" takes patient "+ currentPatient + " to doctor's office." );
-                patientInOfficeID = currentPatient;
-
-            } catch(InterruptedException e) {
+                Thread.sleep(1000);
+                new Patient(i);
+            }catch(InterruptedException e) {
                 System.out.println("InterruptedException caught");
             }
 
-        } catch(InterruptedException e) {
-            System.out.println("InterruptedException caught");
         }
-        doctorSem.release();
 
     }
 
-    public void doctorConsume(int DoctorID){
-        try{
-            doctorSem.acquire();
-            System.out.println("Doctor " +DoctorID+ " listens to symptoms from patient "+ patientInOfficeID);
-            Thread.sleep(500);
-        } catch(InterruptedException e) {
-            System.out.println("InterruptedException caught");
+    /**************************************************************************/
+    /**************************************************************************/
+
+    // Patient class
+    class Patient implements Runnable{
+        int PatientID;
+        boolean seenReceptionist;
+        boolean exited;
+        Semaphore seenReceptionistSem = new Semaphore(0);
+        Semaphore seenDoctorSem = new Semaphore(0);
+        int doctorSeen;
+
+        Patient(int i){
+            PatientID = i;
+            seenReceptionist = false;
+            exited = false;
+            Thread t = new Thread(this, "Doctor");
+            t.setDaemon(true);
+            t.start();
         }
-        nurseSem.release();
-    }
-}
-/******************************************************************************/
-/******************************************************************************/
 
-// Patient class
-class Patient implements Runnable{
-    DoctorsOffice q;
-    int PatientID;
-    boolean seenReceptionist;
+        @Override
+        public void run() {
 
-    Patient(DoctorsOffice q, int i){
-        this.q = q;
-        PatientID = i;
-        seenReceptionist = false;
-        new Thread(this, "Patient "+PatientID).start();
-    }
+            System.out.println("Patient "+PatientID+" enters waiting room, waits for receptionist");
 
-    @Override
-    public void run() {
-        System.out.println("Patient "+PatientID+" enters waiting room, waits for receptionist" );
-        while (!seenReceptionist){
-            seenReceptionist = q.getReceptionist(PatientID);
-        }
-        q.putWaitingRoom(PatientID);
-//        System.out.println(q.getWaitingRoomListLength());
-    }
+            while (!seenReceptionist){
+                try{
+                    // acquire the receptionist to be checked in
+                    consumeReceptionistSem.acquire();
 
-    public int getPatientId(){
-        return PatientID;
-    }
+                    // note that we HAVE seen the receptionist
+                    seenReceptionist = true;
 
-    public void setSeenReceptionist(boolean b){
-        seenReceptionist = b;
-    }
-}
-/******************************************************************************/
-/******************************************************************************/
+                    // set the global to show that this patient is at the desk
+                    patientAtReceptionist = this;
 
-// Receptionist class
-class Receptionist implements Runnable
-{
-    DoctorsOffice q;
+                     //wait then print the prompt
+                     Thread.sleep(1000);
+                     //System.out.println("Patient "+PatientID+" checks in with the receptionist [unecessary prompt]");
+
+                } catch(InterruptedException e) {
+                    System.out.println("InterruptedException caught");
+                }
+            }
 
 
-    Receptionist(DoctorsOffice q){
-        this.q = q;
-        new Thread(this, "Receptionist ").start();
-    }
+            // Release Receptionist to check in someone else.
+            produceReceptionistSem.release();
+            waitroomList.add(this);
+            waitingRoomSem.release();
 
-    @Override
-    public void run() {
-        while(true){
-            q.putReceptionist();
-        }
-    }
-}
 
-/******************************************************************************/
-/******************************************************************************/
+            while (!exited){
+                try{
+                    seenDoctorSem.acquire();
+                    // wait then print the prompt
+                    Thread.sleep(1000);
+                    System.out.println("Patient "+PatientID+" receives advice from doctor "+doctorSeen);
 
-// Nurse class -- Producer
-class Nurse implements Runnable{
-    DoctorsOffice q;
-    int NurseID;
-    Nurse(DoctorsOffice q, int i) {
-        this.q = q;
-        this.NurseID = i;
-        new Thread(this, "Nurse").start();
-    }
+                    // allow the patient to leave
+                    Thread.sleep(1000);
+                    System.out.println("Patient "+PatientID+" leaves");
+                    exitedList.add(this);
+                    exited = true;
+                    if (exitedList.size()>=NUMPATIENTS){
+                        System.out.println("OFFICE IS NOW CLOSED!");
+                        officeClosed = true;
+                    }
+//                    System.out.println("EXITED ARRAYLIST: "+ exitedList.size());
 
-    @Override
-    public void run() {
-        while(true){
-            q.nurseProduce(NurseID);
+                } catch(InterruptedException e) {
+                    System.out.println("InterruptedException caught");
+                }
+            }
+            //System.out.println("Patient "+PatientID+" gets there");
         }
     }
-}
 
-/******************************************************************************/
-/******************************************************************************/
+    /**************************************************************************/
+    /**************************************************************************/
 
-// Doctor class -- Consumer
-class Doctor implements Runnable{
-    DoctorsOffice q;
-    int DoctorID;
+    // Doctor class
+    class Doctor implements Runnable{
+        int DoctorID;
+        Nurse nurse;
+        int i;
 
-    Doctor(DoctorsOffice q, int i){
-        this.q = q;
-        this.DoctorID = i;
-        new Thread(this, "Doctor").start();
+        // Semaphore to note that doctor is/isnt busy
+        Semaphore doctorSem = new Semaphore(1);
+
+        Doctor(int i){
+            this.DoctorID = i;
+            nurse = new Nurse(DoctorID);
+            Thread t = new Thread(this, "Doctor");
+            t.setDaemon(true);
+            t.start();
+        }
+
+        @Override
+        public void run(){
+            while(i<3){
+                try{
+                    i++;
+                    // set doctor to busy
+                    doctorSem.acquire();
+                    nurse.consumeExamRoom.acquire();
+
+                    // wait then print the prompt
+                    Thread.sleep(1000);
+                    System.out.println("Doctor "+DoctorID+" listens to symptoms from patient "+nurse.currentPatient.PatientID);
+
+                    // let patient know doctor number and that that s/he should be listening to advice from doctor
+                    nurse.currentPatient.doctorSeen = DoctorID;
+                    nurse.currentPatient.seenDoctorSem.release();
+
+                    // set as no longer busy
+                    doctorSem.release();
+
+                    // set nurse to no longer busy
+                    nurse.nurseSem.release();
+
+                    // patient leaves exam room, release for nurse to fill again
+                    nurse.produceExamRoom.release();
+
+                } catch(InterruptedException e) {
+                    System.out.println("InterruptedException caught");
+                }
+            }
+            System.out.println("Doctor "+DoctorID+" gets there");
+        }
     }
 
-    @Override
-    public void run()
+    /**************************************************************************/
+    /**************************************************************************/
+
+    // Nurse class
+    class Nurse implements Runnable{
+        int NurseID;
+
+        /*Semaphore to note that nurse is/isnt busy*/
+        Semaphore nurseSem = new Semaphore(1);
+
+
+        Semaphore produceExamRoom = new Semaphore(1);
+        Semaphore consumeExamRoom = new Semaphore(0);
+
+        Patient currentPatient;
+
+        Nurse(int i) {
+            this.NurseID = i;
+            Thread t = new Thread(this, "Nurse");
+            t.setDaemon(true);
+            t.start();
+        }
+
+        @Override
+        public void run() {
+            while(!officeClosed){
+                try{
+                    // grab somone from the waitroom
+                    waitingRoomSem.acquire();
+
+                    // double semaphore to make sure nurse places patient in waitroom before doctor treats
+                    produceExamRoom.acquire();
+
+                    // set myself to busy
+                    nurseSem.acquire();
+
+                    currentPatient = waitroomList.remove(0);
+
+                    // wait then print the prompt
+                    Thread.sleep(1000);
+                    System.out.println("Nurse "+NurseID+" takes patient "+currentPatient.PatientID+" to doctor's office");
+
+                    // okay doctor, you can now see the patient
+                    consumeExamRoom.release();
+
+                } catch(InterruptedException e) {
+                    System.out.println("InterruptedException caught");
+                }
+            }
+            System.out.println("Nurse "+NurseID+" gets there");
+        }
+    }
+
+    /**************************************************************************/
+    /**************************************************************************/
+
+    // Receptionist class
+    class Receptionist implements Runnable
     {
-        while(true){
-            q.doctorConsume(DoctorID);
+        /*Semaphore to note that doctor is/isnt busy*/
+        Semaphore receptionistSem = new Semaphore(1);
+
+        Receptionist(){
+            Thread t = new Thread(this, "Doctor");
+            t.setDaemon(true);
+            t.start();
+        }
+
+        @Override
+        public void run() {
+            while(!officeClosed){
+                try{
+                    produceReceptionistSem.acquire();
+
+                    // wait then print the prompt
+                    Thread.sleep(1000);
+                    System.out.println("Receptionist registers patient "+ patientAtReceptionist.PatientID);
+                    patientAtReceptionist.seenReceptionist = true;
+                } catch(InterruptedException e) {
+                    System.out.println("InterruptedException caught");
+                }
+                consumeReceptionistSem.release();
+            }
+            System.out.println("Receptionist gets there");
         }
     }
-}
 
-/******************************************************************************/
-/******************************************************************************/
+    /**************************************************************************/
+    /**************************************************************************/
 
-// Driver class
-class PC
-{
     public static void main(String args[])
     {
-        // creating buffer queue
-        DoctorsOffice q = new DoctorsOffice();
+        //set number of patients and doctors
+        int p = 5;
+        int d = 3;
 
-        for (int i = 1; i < 2; i++) {
-            // starting consumer thread
-            new Doctor(q, i);
+        System.out.println("Run with "+ p+ " patients, "+ d+" nurses, "+ d+" doctors");
+        System.out.println("");
 
-            // starting producer thread
-            new Nurse(q, i);
-        }
-
-
-        // starting receptionist thread
-        new Receptionist(q);
-
-        // starting patient thread
-        for (int i = 1; i < 4; i++) {
-           new Patient(q, i);
-           try {
-                sleep(1000);
-            }
-            catch (InterruptedException e) {
-            }
+        DoctorsOffice doctorsOffice = new DoctorsOffice(p, d);
+        while(!doctorsOffice.officeClosed){
+            //we are still running
         }
     }
 }
+
+
+/******************************************************************************/
+/******************************************************************************/
